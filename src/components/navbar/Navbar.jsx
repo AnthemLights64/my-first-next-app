a8c6d5d1d38fbb98cee0e8fa8c082f949a973f27
wenlonglu <yummybroccoli@outlook.com>
2023-08-04 19:22:15 +0800

    style: Navbar and Footer styled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import React from 'react'
-import styles from './page.module.css'
+import styles from './navbar.module.css'
 
 const links = [
     {
@@ -40,7 +40,7 @@ const links = [
 const Navbar = () => {
   return (
     <div className={styles.container}>
-        <Link href="/" className={styles.logo}>to home page</Link>
+        <Link href="/" className={styles.logo}>Aurora</Link>
         <div className={styles.links}>
             {links.map(link => (
                 <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
