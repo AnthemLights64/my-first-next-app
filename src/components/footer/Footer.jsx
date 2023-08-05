89879279f02e4f6e2d9a7f9b16ace4cc8c22001c
wenlonglu <yummybroccoli@outlook.com>
2023-08-05 19:29:32 +0800

    style: add social media to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
         <div>©2023 WenlongLu. All rights reserved.</div>
-        <div></div>
+        <div className={styles.social}>
+            <Image src="/facebook.png" width={15} className={styles.icon} height={15} alt=''/>
+            <Image src="/instagram.png" width={15} className={styles.icon} height={15} alt=''/>
+            <Image src="/twitter.png" width={15} className={styles.icon} height={15} alt=''/>
+            <Image src="/youtube.png" width={15} className={styles.icon} height={15} alt=''/>
+        </div>
     </div>
   )
 }
