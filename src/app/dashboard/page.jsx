98ccc00bc75f5a68e8cd52a597ff3b19ac8ae218
wenlonglu <yummybroccoli@outlook.com>
2023-08-07 00:57:52 +0800

    feat: authenticated with Google

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
 import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
 const Dashboard = () => {
 
@@ -28,6 +29,9 @@ const Dashboard = () => {
   //   };
   //   getData();
   // }, []);
+
+  const session = useSession()
+  console.log(session)
 
   const fetcher = (...args) => fetch(...args).then(res => res.json());
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
